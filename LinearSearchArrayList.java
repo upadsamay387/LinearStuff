3b257a2fc44f188caa8f5dcc8275c0add92bc06e
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2020-10-30 17:59:00 -0500

    Update LinearSearchArrayList.java

--- LinearSearchArrayList.java
+++ LinearSearchArrayList.java
@@ -1,6 +1,6 @@
 public int LinearSearch(ArrayList<Integer> arr, int val) {
-   if (arr.contains(val)) {
+   if (arr.contains(val))
       for(int x = 0; x<arr.size(); x++)
          if(arr.get(x).equals(val)) return val;
    return -1;
-   }
+}

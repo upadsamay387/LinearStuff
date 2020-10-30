a0be0f4d1bf34f289232674cbfeab0d2b4969ee7
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2020-10-30 17:52:35 -0500

    Create LinearTraversalArrayList2D.java

---
+++ LinearTraversalArrayList2D.java
@@ -0,0 +1,7 @@
+public String LinearSearch(ArrayList<ArrayList> arr, Object val) {
+   for(int k = 0; k<arr.size(); k++)
+       if (arr.get(k).contains(val))
+          for(int x = 0; x<arr.get(k).size(); x++)
+              if(arr.get(k).get(x).equals(val)) return "[" + k + "," + x + "]";
+   return "" + -1;
+   }

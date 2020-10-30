82cd433d443b4ee87b8cec013290716b22385bec
upadsamay387 <56898833+upadsamay387@users.noreply.github.com>
2020-10-30 17:26:35 -0500

    Update and rename LinearSearch.java to LinearSearchStatic.java

--- LinearSearchStatic.java
+++ LinearSearchStatic.java
@@ -1,4 +1,4 @@
-public int LinearSearch(int[] arr, int val) {
+public int LinearSearchStatic(int[] arr, int val) {
    if (arr.contains(val)) {
       for(int x = 0; x<arr.size; x++)
          if(arr[x] == val) return val;
